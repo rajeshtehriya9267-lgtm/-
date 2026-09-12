d0edf3cc30dcb3c32e5336dadb8efa838ab95e3d
rajeshtehriya9267-lgtm <rajeshtehriya9267@gmail.com>
2026-09-12 07:04:16 +0530

    admins.py

---
+++ database/admins.py
@@ -0,0 +1,21 @@
+from .database import db
+
+
+class AdminsDB:
+    def __init__(self):
+        self.col = db.admins
+
+    async def add(self, user_id: int):
+        await self.col.update_one({"user_id": user_id}, {"$set": {"admin": True}}, upsert=True)
+
+    async def remove(self, user_id: int):
+        await self.col.delete_one({"user_id": user_id})
+
+    async def all_ids(self) -> list[int]:
+        return [a["user_id"] async for a in self.col.find({})]
+
+    async def is_admin(self, user_id: int) -> bool:
+        return bool(await self.col.find_one({"user_id": user_id}))
+
+
+db_admins = AdminsDB()
